53bb8c4206242c00f1babba660fa3d054bb5b76d
Long Dinh <longtd.22ite@vku.udn.vn>
2024-05-05 15:15:53 +0700

    #fix remove using

--- WavesOfFoodDemo.Server/Program.cs
+++ WavesOfFoodDemo.Server/Program.cs
@@ -1,8 +1,3 @@
-using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using System;
 using WavesOfFoodDemo.Server.AppSettings;
 using WavesOfFoodDemo.Server.DataContext;
 
